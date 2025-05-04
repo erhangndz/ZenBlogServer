20b2b5adb5299a5831ed1156ad99228a805119e9
Erhan Gündüz <erhangndz@live.com>
2025-05-04 15:04:18 +0300

    Add project files.

---
+++ Infrastructure/ZenBlog.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZenBlog.Domain.Entities;
+
+namespace ZenBlog.Persistence.Context
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<ContactInfo> ContactInfos { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Social> Socials { get; set; }
+
+
+    }
+}
